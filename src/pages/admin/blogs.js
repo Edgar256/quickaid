4f614f2d2f943df299176d8f970e73cb365a7d98
edgar256 <tinka.edgar@gmail.com>
2024-07-08 14:00:53 +0300

    ft - Added and Intergrated Delete blog API

--- src/pages/admin/blogs.js
+++ src/pages/admin/blogs.js
@@ -53,7 +53,7 @@ export default function index() {
 
           <main className="col-md-9 ms-sm-auto col-lg-10 px-md-4">
             <div className="d-flex justify-content-between flex-wrap flex-md-nowrap align-items-center pt-3 pb-2 mb-3 border-bottom">
-              <h3 className="h2">Blogs</h3>
+              <h3 className="h2">Blogs({blogs.length})</h3>
             </div>
             {error && (
               <div className="alert alert-danger text-center">{error}</div>
